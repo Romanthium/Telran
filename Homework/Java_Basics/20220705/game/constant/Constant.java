2ed6d0fe41d36888652a4d4fe28e7dc177690201
Roman <romanthium@gmail.com>
2022-07-02 13:44:33 +0200

    Create Constant.java

---
+++ Homework/Java_Basics/20220705/game/constant/Constant.java
@@ -0,0 +1,12 @@
+package game.hero.constant;
+
+public class Constant {
+    public static final int ELF_POWER = 100;
+    public static final int ELF_CHARISMA = 65;
+    public static final int ELF_MAGIC = 0;
+
+    public static final int ORC_POWER = 200;
+    public static final int ORC_HEALTH = 100;
+    public static final int ORC_MAGIC = 0;
+
+}
