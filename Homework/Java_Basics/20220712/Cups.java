d036c455b80caf76a1273cdbc0d323c7fcba29d7
Roman <romanthium@gmail.com>
2022-07-07 21:54:40 +0200

    Create Cups.java

---
+++ Homework/Java_Basics/20220712/Cups.java
@@ -0,0 +1,11 @@
+package com.telran.berlin.practice.homework0707;
+
+public class Cups {
+    public static void main(String[] args) {
+        float f = 3.9f;
+        String s = "cups";
+
+        System.out.println((int)f + " " + s);
+        System.out.println(Math.round(f) + " " + s);
+    }
+}
