c1b4fd875741c04949f6d0dad50ba507b01be7c0
Roman <romanthium@gmail.com>
2022-07-11 21:51:43 +0200

    Create Student.java

---
+++ Homework/Java_Basics/20220714/Student.java
@@ -0,0 +1,19 @@
+package com.telran.berlin.practice.homework0707;
+
+public class Student {
+    private String name;
+    private int age;
+    private String address;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Student(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+    
+}
